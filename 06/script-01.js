b4fde8587f31d5a6c503a931bac16bcc24a0047d
mattrcole <matthewrichardcole@gmail.com>
2023-12-05 22:09:33 -0700

    day 6 part 1

---
+++ 06/script-01.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+const file = fs.readFileSync('./06/input.txt', { encoding: 'utf-8'})
+
+const [timeStr, distanceStr] = file.split('\n')
+
+const times = timeStr.slice('Time: '.length).split(' ').filter(({ length }) => length).map(n => parseInt(n))
+const distances = distanceStr.slice('Distance: '.length).split(' ').filter(({ length }) => length).map(n => parseInt(n))
+
+
+const timesAndDistances = times.reduce((acc, time, i) => ([...acc, { time, distance: distances[i] }]), [])
+
+
+const getTotalDistanceBreakdown = ({ time, distance }) => {
+    const winningDistances = []
+    for (i = 1; i < time - 1; i++) {
+        const remainingTime = time - i
+        const totalDistance = i * remainingTime
+        if (totalDistance > distance) winningDistances.push(totalDistance)
+    }
+
+    return winningDistances
+}
+console.log(timesAndDistances.map(td => getTotalDistanceBreakdown(td)).reduce((acc, { length }) => acc * length, 1))
+
+
