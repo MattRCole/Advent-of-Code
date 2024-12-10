edb2b3d12debd2e0b9c9e88144c77f5c36c64bc9
Matthew Cole <matthewrichardcole@gmail.com>
2024-12-10 10:47:59 -0700

    Clean up 2024:9-2

--- 2024/09/script-02.ts
+++ 2024/09/script-02.ts
@@ -2,35 +2,28 @@
 
 import { getArray, splice, sum } from "../util/array.ts";
 
+const descendingOrder = (a: number, b: number) => b - a;
+
 const parseInput = (file: string) => {
-  // const gapSizes: { [size: number]: number[] }
-  const filledSizes: { [size: number]: number[] } = [];
-  const hardDriveIndexes = [] as number[];
-  const gaps = [] as number[];
-  const filled = [] as number[];
-  let hardDriveIndex = 0;
-  // const
+  const fileSizeToIdMap: { [size: number]: number[] } = [];
+  const freeSpaceBlocks = [] as number[];
+  const fileSizes = [] as number[];
 
   file.split("").forEach((char, idx) => {
     const num = parseInt(char);
     // const realIndex = Math.floor(idx / 2)
 
     if (idx % 2 === 0) {
-      const realIndex = filled.length;
-      filled.push(num);
-      filledSizes[num] = [...(filledSizes[num] || []), realIndex].toSorted((
-        a,
-        b,
-      ) => b - a);
-      hardDriveIndexes.push(hardDriveIndex);
+      const fileId = fileSizes.length;
+      fileSizes.push(num);
+      fileSizeToIdMap[num] = [...(fileSizeToIdMap[num] || []), fileId].toSorted(
+        descendingOrder,
+      );
     } else {
-      // const realIndex = gaps.length
-      gaps.push(num);
-      // gapSizes[num] = [...(gapSizes[num] || []), realIndex].toSorted((a, b) => a - b)
+      freeSpaceBlocks.push(num);
     }
-    hardDriveIndex += num;
   });
-  return { gaps, filledSizes, filled, hardDriveIndexes };
+  return { freeSpaceBlocks, fileSizeToIdMap, fileSizes };
 };
 
 const getNumericKeysWithoutUndefinedKeys = <
@@ -41,89 +34,88 @@ const getNumericKeysWithoutUndefinedKeys = <
   );
 };
 
-const getLargestIndexAndSize = (
-  target: number,
-  filledSizes: { [size: number]: number[] | undefined },
-): { size: number; index: number } => {
-  const validSizes = getNumericKeysWithoutUndefinedKeys(filledSizes).filter(
-    (size) => size <= target,
-  );
+const getLargestFileIdWithAcceptableSize = (
+  targetSize: number,
+  fileSizeToIdMap: { [size: number]: number[] | undefined },
+): { size: number; fileId: number } => {
+  const availableSizes = getNumericKeysWithoutUndefinedKeys(fileSizeToIdMap)
+    .filter(
+      (size) => size <= targetSize,
+    );
 
-  let maxId = -1;
-  let sizeToUse = -1;
-  for (const size of validSizes) {
-    const ids = filledSizes[size];
-    if (ids === undefined) continue;
+  let maxFileId = -1;
+  let maxFileIdSize = -1;
+  for (const size of availableSizes) {
+    const fileIds = fileSizeToIdMap[size];
+    if (fileIds === undefined) continue;
 
-    const potentialId = ids[0];
-    if (potentialId > maxId) {
-      maxId = potentialId;
-      sizeToUse = size;
+    const potentialFileId = fileIds[0];
+    if (potentialFileId > maxFileId) {
+      maxFileId = potentialFileId;
+      maxFileIdSize = size;
     }
   }
 
-  return { size: sizeToUse, index: maxId };
+  return { size: maxFileIdSize, fileId: maxFileId };
 };
 
 const removeItem = (arr: number[], toRemove: number) =>
   splice(arr, arr.indexOf(toRemove), 1);
 
-const fragmentInput = (
-  input: {
-    gaps: number[];
-    filled: number[];
-    filledSizes: { [size: number]: number[] };
-    hardDriveIndexes: number[];
-  },
+const organizeDiskLocations = (
+  input: ReturnType<typeof parseInput>,
 ): number[] => {
-  const filledSizes: { [size: number]: number[] | undefined } = {
-    ...input.filledSizes,
+  const fileSizeToIdMap: { [size: number]: number[] | undefined } = {
+    ...input.fileSizeToIdMap,
   };
   const result = [] as number[];
-  let gapRemainder = 0;
-  let gapIndex = -1;
-  const usedNumberIndexes: Set<number> = new Set();
-  while (gapIndex < input.gaps.length) {
-    if (gapRemainder <= 0) {
-      gapIndex++;
+  let remainingFreeSpaceInBlock = 0;
+  let freeSpaceBlockIndex = -1;
+  const usedFileIds: Set<number> = new Set();
+  while (freeSpaceBlockIndex < input.freeSpaceBlocks.length) {
+    if (remainingFreeSpaceInBlock <= 0) {
+      freeSpaceBlockIndex++;
       result.push(
         ...getArray(
-          input.filled[gapIndex],
-          usedNumberIndexes.has(gapIndex) ? 0 : gapIndex,
+          input.fileSizes[freeSpaceBlockIndex],
+          usedFileIds.has(freeSpaceBlockIndex) ? 0 : freeSpaceBlockIndex,
         ),
       );
 
-      usedNumberIndexes.add(gapIndex);
-      const usedSize = input.filled[gapIndex];
-      const ids = filledSizes[usedSize];
-      if (ids !== undefined) {
-        const remainingIds = removeItem(ids, gapIndex);
-        filledSizes[usedSize] = remainingIds.length ? remainingIds : undefined;
+      usedFileIds.add(freeSpaceBlockIndex);
+      const usedSize = input.fileSizes[freeSpaceBlockIndex];
+      const fileIds = fileSizeToIdMap[usedSize];
+      if (fileIds !== undefined) {
+        const remainingIds = removeItem(fileIds, freeSpaceBlockIndex);
+        fileSizeToIdMap[usedSize] = remainingIds.length
+          ? remainingIds
+          : undefined;
       }
 
-      gapRemainder = input.gaps[gapIndex];
+      remainingFreeSpaceInBlock = input.freeSpaceBlocks[freeSpaceBlockIndex];
     }
 
-    const { size: usedSize, index: usedNumIndex } = getLargestIndexAndSize(
-      gapRemainder,
-      filledSizes,
-    );
-    const ids = filledSizes[usedSize];
-    if (ids === undefined) {
-      result.push(...getArray(gapRemainder, 0));
-      gapRemainder = 0;
+    const { size: usedSize, fileId: usedFileId } =
+      getLargestFileIdWithAcceptableSize(
+        remainingFreeSpaceInBlock,
+        fileSizeToIdMap,
+      );
+    const fileIds = fileSizeToIdMap[usedSize];
+    if (fileIds === undefined) {
+      result.push(...getArray(remainingFreeSpaceInBlock, 0));
+      remainingFreeSpaceInBlock = 0;
       continue;
     }
 
-    const newGapRemainder = gapRemainder - usedSize;
-    gapRemainder = newGapRemainder;
-    const remainingIndexes = removeItem(ids, usedNumIndex);
+    const newGapRemainder = remainingFreeSpaceInBlock - usedSize;
+    remainingFreeSpaceInBlock = newGapRemainder;
+    const remainingFileIds = removeItem(fileIds, usedFileId);
 
-    filledSizes[usedSize] = remainingIndexes.length
-      ? remainingIndexes
+    fileSizeToIdMap[usedSize] = remainingFileIds.length
+      ? remainingFileIds
       : undefined;
-    result.push(...getArray(usedSize, usedNumIndex));
-    usedNumberIndexes.add(usedNumIndex);
+    result.push(...getArray(usedSize, usedFileId));
+    usedFileIds.add(usedFileId);
   }
 
   return result;
@@ -132,8 +124,8 @@ const fragmentInput = (
 if (import.meta.main) {
   const file = Deno.readTextFileSync("./09/input.txt");
   const input = parseInput(file);
-  const rawResult = fragmentInput(input);
-  const result = sum(rawResult.map((id, idx) => id * idx));
+  const rawResult = organizeDiskLocations(input);
+  const result = sum(rawResult.map((fileId, idx) => fileId * idx));
   console.log({
     res: result,
   });
